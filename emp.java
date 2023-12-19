d6d08db57fa6778838641c35fd8bc701d7c36de1
aameensiddiqui <154116549+aameensiddiqui@users.noreply.github.com>
2023-12-19 08:25:25 +0530

    Create emp.java

---
+++ emp.java
@@ -0,0 +1,36 @@
+package Poly;
+
+public class Emp extends Person
+{
+		public int empid;
+		public double salary;
+		
+		public Emp() 
+		{
+			super();
+		}
+		public Emp(String name, int dd, int mm, int yy, int empid, double salary)
+		{
+			super(name, dd, mm, yy);
+			this.empid=empid;
+			this.salary=salary;
+		}
+//		public void display()
+//		{
+//			super.display();
+//			System.out.println("\nEMPLOYEE: "+empid+"\nSALARY: "+salary);
+//			System.out.println("*************************************************************************");
+//		}
+		public String toString()
+		{
+			return super.toString()+"\nEMPLOYEE: "+empid+"\nSALARY: "+salary;
+		}
+		public double calSalary()
+		{
+			double gross_salary = salary;
+			return gross_salary;
+		}
+		
+
+	}
+
